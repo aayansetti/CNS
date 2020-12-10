ac75f8794a4682ec0df62645f422660a0786bfe8
aayansetti <57705356+aayansetti@users.noreply.github.com>
2020-12-11 00:32:28 +0530

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -2,74 +2,33 @@
 #include<string.h>
 void main()
 {
-    int i,j,c=0,n,m,z;
-    char a[60],b[60],p[60],d[60];
-    printf("Enter the size of array:");
-    scanf("%d",&n);
-    printf("Enter values of array of A:");
-    for(i=0;i<n;i++)
-    {
-        scanf("%c",&a[i]);
-    }
-    printf("Enter values of array of B:");
-    for(i=0;i<n;i++)
-    {
-        scanf("%c",&b[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<i;j++)
-        {
-            if(b[i]==b[j])
-            c++;
-        }
-    }
-    if(c==0)
-    {
-        printf("Enter the size of plaintext:");
-        scanf("%d",&z);
-        printf("Enter  plaintext:");
-        for(i=0;i<z;i++)
-        {
-        scanf("%c",&p[i]);
-        }
-        printf("Encryption:");
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(p[i]==a[j])
-                {
-                    m=j;
-                }
-            }    
-                d[i]=b[m];
-        }
-        for(i=0;i<z;i++)
-        {
-         printf("%c",d[i]);   
-        }
-        printf("\nDecryption:");
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(d[i]==b[j])
-                {
-                    m=j;
-                }
-            }
-             p[i]=a[m];
-        }
-        for(i=0;i<z;i++)
-        {
-         printf("%c",p[i]);   
-        }
-        
-    }
-    else
-    for(i=0;i<z;i++)
-    {
-        printf("%c already existed",b[i]);
-    }
+int i,k, v[30];
+char c,enc[30],pt[50];
+for(i=0;i<26;i++)
+{
+v[i]=0;
+}
+i=0;
+printf("Enter the plain text: ");
+scanf("%s",pt);
+printf("Enter the encryption values for each alphabet: \n");
+while(i<26)
+{
+printf("%c: ",97+i);
+scanf(" %c",&enc[i]);
+k=enc[i]-97;
+if(v[k]==1)
+{
+printf("Enter a value which is not assigned to another alphabet\n");
+continue;
+}
+v[k]=1;
+i++;
+}
+printf("The encrypted meassage is:    ");
+for(i=0;i<strlen(pt);i++)
+{
+printf("%c",enc[pt[i]-97]);
+}
+printf("\n");
 }
